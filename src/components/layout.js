bafcfa6dd7598981e23093091e15512c784b5866
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-10-15 11:56:18 +0530

    Menu Hihglighted

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,71 +6,30 @@ import "./layout.css"
 
 const Layout = ({ children }) => {
   useEffect(() => {
-    // $(document).ready(function () {
-    //   setTimeout(function () {
-    //     $(".content").fadeOut();
-    //   }, );
-    // });
-
-    $(document).ready(function() {
-      $(".navbar-nav").click(function() {
-        $("#navbarSupportedContent").close()
-      })
-    })
-
-    // Cookie Strip
-    $(document).ready(function() {
-      $("#hide").click(function() {
-        $(".content").hide()
-      })
-    })
-
-    $(".dropdown-menu a.dropdown-toggle").on("click", function(e) {
-      if (
-        !$(this)
-          .next()
-          .hasClass("show")
-      ) {
-        $(this)
-          .parents(".dropdown-menu")
-          .first()
-          .find(".show")
-          .removeClass("show")
-      }
-      var $subMenu = $(this).next(".dropdown-menu")
-      $subMenu.toggleClass("show")
-
-      // NavLink HighLight
-      $(this)
-        .parents("li.nav-item.dropdown.show")
-        .on("hidden.bs.dropdown", function(e) {
-          $(".dropdown-submenu .show").removeClass("show")
-        })
-    })
-
-    $(document).ready(function() {
-      $(function() {
-        $(".dropdown-item").on("click", function(e) {
-          $(".nav-item .nav-link").addeClass("active")
+    $(function ($) {
+      let url = window.location.href
+      $(".dropdown-item").each(function () {
+        if (this.href === url) {
           $(this)
             .parents(".nav-item")
             .find(".nav-link")
             .addClass("active")
-        })
+        }
       })
     })
 
-    //     $(document).ready(function() {
-    //    $('#otherservice').hide();
-    //    $("#otherservice").style.display = "none";
-    // });
-    //     $("#otherservice").hide();
+    // Cookie Strip
+    $(document).ready(function () {
+      $("#hide").click(function () {
+        $(".content").hide()
+      })
+    })
 
-    $(document).ready(function() {
+    $(document).ready(function () {
       $("#otherservice").hide()
     })
     // $("#otherservice").style.display = "none";
-    $("#service").change(function() {
+    $("#service").change(function () {
       var val = $(this).val()
       $("#otherservice").hide()
       if (val == "Others") {
@@ -81,10 +40,10 @@ const Layout = ({ children }) => {
       }
     })
 
-    $(document).ready(function() {
+    $(document).ready(function () {
       $("#othersource").hide()
     })
-    $("#source").change(function() {
+    $("#source").change(function () {
       var val = $(this).val()
       $("#othersource").hide()
       if (val == "Othersource") {
